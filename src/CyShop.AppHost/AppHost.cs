23fb0b0fea67308ccafab6192cb48b96960858d9
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2026-04-14 14:45:17 +0800

    Add Basket API

--- src/CyShop.AppHost/AppHost.cs
+++ src/CyShop.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Catalog_API>("catalog-api");
 
+builder.AddProject<Projects.Basket_API>("basket-api");
+
 builder.Build().Run();
